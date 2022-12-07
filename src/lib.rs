b815441930b39cc542aec12883b00b8462cd4bed
Calum <calum@mackenzie-chambers.co.uk>
2022-12-07 15:30:40 +0000

    fix: Runnable for all days

--- src/lib.rs
+++ src/lib.rs
@@ -28,10 +28,36 @@ pub fn run(day: u8, test: Option<bool>) {
 
     let input = load(day, test);
 
-    let solution = match day {
-        1 => solutions::Day1,
-        _ => panic!("Day {} not implemented", day),
+    let solution: Option<Box<dyn Solution>> = match day {
+        1 => Some(Box::new(solutions::Day1)),
+        2 => Some(Box::new(solutions::Day2)),
+        3 => Some(Box::new(solutions::Day3)),
+        4 => Some(Box::new(solutions::Day4)),
+        5 => Some(Box::new(solutions::Day5)),
+        6 => Some(Box::new(solutions::Day6)),
+        7 => Some(Box::new(solutions::Day7)),
+        8 => Some(Box::new(solutions::Day8)),
+        9 => Some(Box::new(solutions::Day9)),
+        10 => Some(Box::new(solutions::Day10)),
+        11 => Some(Box::new(solutions::Day11)),
+        12 => Some(Box::new(solutions::Day12)),
+        13 => Some(Box::new(solutions::Day13)),
+        14 => Some(Box::new(solutions::Day14)),
+        15 => Some(Box::new(solutions::Day15)),
+        16 => Some(Box::new(solutions::Day16)),
+        17 => Some(Box::new(solutions::Day17)),
+        18 => Some(Box::new(solutions::Day18)),
+        19 => Some(Box::new(solutions::Day19)),
+        20 => Some(Box::new(solutions::Day20)),
+        21 => Some(Box::new(solutions::Day21)),
+        22 => Some(Box::new(solutions::Day22)),
+        23 => Some(Box::new(solutions::Day23)),
+        24 => Some(Box::new(solutions::Day24)),
+        25 => Some(Box::new(solutions::Day25)),
+        _ => None,
     };
+
+    let solution = solution.expect("No solution found for this day");
 
     let part_a_time = std::time::Instant::now();
     let part_a = solution.part_a(&input);
